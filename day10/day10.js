744306bd32d661d4c082cd137da92350dfcb77ab
Chaz <chazlouviere@gmail.com>
2021-12-10 09:57:38 -0500

    Day10 part 2 complete.

--- day10/day10.js
+++ day10/day10.js
@@ -28,25 +28,28 @@ let input = fs.readFileSync(filename).toString("utf-8");
 input = input.split("\n");
 input = input.filter((e) => e);
 
-console.log("---Part 1---");
-calculatePartOne();
-console.log("---Part 2---");
-calculatePartTwo();
+//console.log("---Part 1---");
+//calculatePartOne();
+//console.log("---Part 2---");
+calculatePartOneAndTwo();
 
 function numSort(a, b) {
   return +a - +b;
 }
 
-function calculatePartOne() {
+function calculatePartOneAndTwo() {
   //console.log(input);
-  let total = 0;
-  for (let i = 0; i < input.length; i++) {
+  let total1 = 0;
+  let total2 = 0;
+  let closingPointsArray = [];
+  lineLoop: for (let i = 0; i < input.length; i++) {
     const line = input[i].split("");
     let openArray = [],
       closeArray = [];
     let lastOpen = "";
+    let closingPoints = 0;
     //console.log(line);
-    for (let c = 0; c < line.length; c++) {
+    charLoop: for (let c = 0; c < line.length; c++) {
       const character = line[c];
       let isIncorrect = false;
       let incPointValue = 0;
@@ -54,49 +57,97 @@ function calculatePartOne() {
         case ")":
           incPointValue = 3;
           isIncorrect = checkOpen("(", character);
-          closeArray.push(character);
+          //closeArray.push(character);
           break;
         case "]":
           incPointValue = 57;
           isIncorrect = checkOpen("[", character);
-          closeArray.push(character);
+          //closeArray.push(character);
           break;
         case "}":
           incPointValue = 1197;
           isIncorrect = checkOpen("{", character);
-          closeArray.push(character);
+          //closeArray.push(character);
           break;
         case ">":
           incPointValue = 25137;
           isIncorrect = checkOpen("<", character);
-          closeArray.push(character);
+          //closeArray.push(character);
           break;
 
         default:
+          switch (character) {
+            case "(":
+              closeArray.unshift(")");
+              break;
+            case "[":
+              closeArray.unshift("]");
+              break;
+            case "{":
+              closeArray.unshift("}");
+              break;
+            case "<":
+              closeArray.unshift(">");
+              break;
+          }
           lastOpen = character;
           openArray.push(character);
           break;
       }
 
       if (isIncorrect) {
-        total += incPointValue;
-        break;
+        total1 += incPointValue;
+        continue lineLoop;
       }
     }
     //console.log(openArray);
     //console.log(closeArray);
 
+    // calculate value of close array
+    for (let i = 0; i < closeArray.length; i++) {
+      const character = closeArray[i];
+      let basePoints = 0;
+      switch (character) {
+        case ")":
+          basePoints = 1;
+          break;
+        case "]":
+          basePoints = 2;
+          break;
+        case "}":
+          basePoints = 3;
+          break;
+        case ">":
+          basePoints = 4;
+          break;
+      }
+
+      closingPoints = closingPoints * 5 + basePoints;
+      //console.log(closingPoints);
+    }
+
+    closingPointsArray.push(closingPoints);
+
     function checkOpen(lookForChar, inChar) {
-			let openChar = openArray[openArray.length - 1];
+      let openChar = openArray[openArray.length - 1];
       if (openChar === lookForChar) {
-				openArray.pop();
+        openArray.pop();
+        closeArray.shift();
         return false;
       } else {
-				//console.log("Invalid character: " + inChar);
+        //console.log("Invalid character: " + inChar);
         return true;
       }
     }
   }
-	console.log(total);
+
+	//console.log(closingPointsArray);
+  closingPointsArray = closingPointsArray.sort(numSort);
+	//console.log(closingPointsArray);
+  total2 = closingPointsArray[Math.floor(closingPointsArray.length / 2)];
+
+  console.log("---Part 1---");
+  console.log(total1);
+  console.log("---Part 2---");
+  console.log(total2);
 }
-function calculatePartTwo() {}
