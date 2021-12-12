bee624c47c2e5588e4f87f4e07edbcc9ad08388c
Chaz <chazlouviere@gmail.com>
2021-12-11 19:04:52 -0500

    Remove comments and octopidictionary code.

--- day11/day11.js
+++ day11/day11.js
@@ -33,23 +33,11 @@ input = input.split("\n");
 input = input.filter((e) => e);
 
 let octopiMatrix = new Array();
-let octopiDict = new Object();
 
 input.forEach((line) => {
   let lineArray = line.split("").map(Number);
   octopiMatrix.push(lineArray);
 });
-
-for (let y = 0; y < input.length; y++) {
-  for (let x = 0; x < input[y].length; x++) {
-    const energyLevel = parseInt(input[y][x]);
-    //let lineArray = line.split("").map(Number);
-    octopiDict[`${x},${y}`] = energyLevel;
-  }
-}
-
-//console.log(octopiMatrix);
-//console.log(octopiDict);
 
 console.log("---Part 1---");
 calculatePartOne();
@@ -61,9 +49,6 @@ function numSort(a, b) {
 }
 
 function calculatePartOne() {
-  //console.log("Before any steps:");
-  //console.log(octopiMatrix);
-
   let totalFlashes = 0;
   let firstFlashSync = -1;
 
@@ -77,8 +62,6 @@ function calculatePartOne() {
         incrementOctopi(position);
       }
     }
-    //console.log(`\nAfter incrementing step: ${step}`);
-    //console.log(octopiMatrix);
 
     // 2. Flash the correct octopi
     for (let i = 0; i < octopiToFlash.length; i++) {
@@ -93,11 +76,7 @@ function calculatePartOne() {
         bl = [octPositionX - 1, octPositionY + 1],
         b = [octPositionX, octPositionY + 1],
         br = [octPositionX + 1, octPositionY + 1];
-      //console.log(octPosition);
-      //console.log(octPosition);
 
-      let octEnergy = octopiMatrix[octPositionY][octPositionX];
-      //console.log(`${octEnergy}`);
       // add 1 to the 8 possible surrounding octopi
       // top left
       incrementOctopi(tl);
@@ -116,9 +95,6 @@ function calculatePartOne() {
       // bottom right
       incrementOctopi(br);
     }
-
-    //console.log(`\nAfter flashing step: ${step}`);
-    //console.log(octopiMatrix);
 
     // 3. Set octopi above 9 back to 0
     for (let y = 0; y < octopiMatrix.length; y++) {
@@ -139,10 +115,7 @@ function calculatePartOne() {
       }
     }
 
-    // at the end
-    //console.log(`\nAfter step: ${step}`);
-    //console.log(octopiMatrix);
-
+    // Part 1 solution
     if (step === 100) {
       console.log(totalFlashes);
       console.log("---Part 2---");
@@ -151,8 +124,6 @@ function calculatePartOne() {
     function incrementOctopi(position) {
       let x = parseInt(position[0]),
         y = parseInt(position[1]);
-      //console.log(x);
-      //console.log(y);
       if (
         x >= 0 &&
         y >= 0 &&
@@ -175,8 +146,6 @@ function calculatePartOne() {
       }
     }
   }
-  //console.log(totalFlashes);
-  //console.log(firstFlashSync);
 }
 
 function calculatePartTwo() {}
