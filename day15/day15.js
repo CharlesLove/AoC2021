25446f05d780e358dca9b2df319034738b401d4d
Chaz <chazlouviere@gmail.com>
2021-12-15 10:38:34 -0500

    Work on A* algorithm.

--- day15/day15.js
+++ day15/day15.js
@@ -5,6 +5,7 @@
 // .txt in memory
 
 // Priority Queue code based on GeekCode
+// A* code based on RedBlobGames
 class QElement {
   constructor(element, priority) {
     this.element = element;
@@ -61,6 +62,21 @@ class PriorityQueue {
   }
 }
 
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+function heuristic(a, b) {
+  let aX = a[0],
+    aY = a[1],
+    bX = b[0],
+    bY = b[1];
+  return Math.abs(aX - bX) + Math.abs(aY - bY);
+}
+
 let fs = require("fs");
 var myArgs = process.argv.slice(2);
 let filename = "";
@@ -104,21 +120,88 @@ function numSort(a, b) {
 }
 
 function calculatePartOne() {
-  let priorityQueue = new PriorityQueue();
-  console.log(priorityQueue.isEmpty());
-  console.log(priorityQueue.front());
+  // let priorityQueue = new PriorityQueue();
+  // console.log(priorityQueue.isEmpty());
+  // console.log(priorityQueue.front());
 
-  priorityQueue.enqueue("Sumit", 2);
-  priorityQueue.enqueue("Gourav", 1);
-  priorityQueue.enqueue("Piyush", 1);
-  priorityQueue.enqueue("Sunny", 2);
-  priorityQueue.enqueue("Sheru", 3);
+  // priorityQueue.enqueue("Sumit", 2);
+  // priorityQueue.enqueue("Gourav", 1);
+  // priorityQueue.enqueue("Piyush", 1);
+  // priorityQueue.enqueue("Sunny", 2);
+  // priorityQueue.enqueue("Sheru", 3);
 
-  console.log(priorityQueue.printPQueue());
-  console.log(priorityQueue.front().element);
-  console.log(priorityQueue.rear().element);
-  console.log(priorityQueue.dequeue().element);
-  priorityQueue.enqueue("Sunil", 2);
-  console.log(priorityQueue.printPQueue());
+  // console.log(priorityQueue.printPQueue());
+  // console.log(priorityQueue.front().element);
+  // console.log(priorityQueue.rear().element);
+  // console.log(priorityQueue.dequeue().element);
+  // priorityQueue.enqueue("Sunil", 2);
+  // console.log(priorityQueue.printPQueue());
+
+  // start and goal arrays are [x, y]
+  let start = new Cell(0, 0);
+  let goal = new Cell(graph[0].length - 1, graph.length - 1);
+
+  console.log(start);
+  console.log(goal);
+
+  let frontier = new PriorityQueue();
+  frontier.enqueue(start, 0);
+  let cameFrom = new Object();
+  let costSoFar = new Object();
+  cameFrom[start] = null;
+  costSoFar[start] = 0;
+
+  while (!frontier.isEmpty()) {
+    let current = frontier.front().element;
+    frontier.dequeue();
+    console.log(current);
+    let curNeighbors = [];
+
+    if (current === goal) {
+      console.log("Goal reached");
+      break;
+    }
+
+    // create neighbors
+    {
+      let aboveNeighbor = new Cell(current.x, current.y - 1),
+        belowNeighbor = new Cell(current.x, current.y + 1),
+        leftNeighbor = new Cell(current.x - 1, current.y),
+        rightNeighbor = new Cell(current.x + 1, current.y);
+      // handle below
+      if (belowNeighbor.y < graph.length) {
+        curNeighbors.push(belowNeighbor);
+      }
+
+      // handle right
+      if (rightNeighbor.x < graph[0].length) {
+        curNeighbors.push(rightNeighbor);
+      }
+
+      // handle above
+      if (aboveNeighbor.y > 0) {
+        curNeighbors.push(aboveNeighbor);
+      }
+
+      // handle left
+      if (leftNeighbor.x > 0) {
+        curNeighbors.push(leftNeighbor);
+      }
+    }
+
+    curNeighbors.forEach((next) => {
+      let curCellCost = graph[current.y][current.x];
+      let nextCellCost = graph[next.y][next.x];
+
+      let newCost = costSoFar[current] + (curCellCost + nextCellCost);
+
+      if (costSoFar[next] === undefined || newCost < costSoFar[next]) {
+        costSoFar[next] = newCost;
+        let priority = newCost + heuristic(goal, next);
+        frontier.enqueue(next, priority);
+        cameFrom[next] = current;
+      }
+    });
+  }
 }
 function calculatePartTwo() {}
